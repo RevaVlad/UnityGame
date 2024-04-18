6da3f36a75db653286afe3ef455c4e34e2bed7f4
RevaVlad <144716008+RevaVlad@users.noreply.github.com>
2024-04-18 16:46:03 +0500

    Temporary ladders

--- Assets/ConnectionNodeScript.cs
+++ Assets/ConnectionNodeScript.cs
@@ -5,36 +5,19 @@ using UnityEngine;
 
 public class ConnectionNodeScript : MonoBehaviour
 {
-    public float force = 0.01f;
-    private Rigidbody2D rb;
-    
     void Start()
     {
-        Debug.Log("Created connection node");
-        rb = transform.parent.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector2(force, 0));
     }
-    public void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        var otherObject = other.gameObject;
-        Debug.Log($"Collision!!! with object {otherObject.name}");
-        
-        if (otherObject.layer == LayerMask.NameToLayer("Ladders"))
-        {
-            var ladderObject = transform.parent.gameObject;
-            Debug.Log($"child: {name}, parent: {ladderObject.name}");
-            //Debug.Log("Connected ladders");
-            otherObject.transform.position = new Vector2(ladderObject.transform.position.x, other.transform.position.y);
-
-            var joint = ladderObject.GetComponent<FixedJoint2D>();
-            joint.enabled = true;
-            joint.connectedBody = otherObject.GetComponent<Rigidbody2D>();
-        }
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ladders") 
+            && (transform.position - other.transform.position).y > 0)
+            transform.parent.GetComponent<LadderScript>().ConnectLadders(other);
     }
-
 }
