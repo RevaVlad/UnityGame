eb6ab598c1d61c32d72ecf40f11309b7f0347b54
Grinik767 <grigorikiseleff@yandex.ru>
2024-04-25 14:11:54 +0500

    add ladder with hidden platform and some fixes

--- Assets/Scripts/ConnectionNodeScript.cs
+++ Assets/Scripts/ConnectionNodeScript.cs
@@ -4,10 +4,10 @@ public class ConnectionNodeScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ladders") 
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ladders")
             && (transform.position - other.transform.position).y > 0)
             transform.parent.GetComponent<LadderScript>().ConnectLadders(other);
-        else if (other.gameObject.layer == LayerMask.NameToLayer("Platforms"))
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Platforms") && !other.gameObject.CompareTag("Hidden"))
             transform.parent.GetComponent<LadderScript>().DestroyConnection();
     }
 }
