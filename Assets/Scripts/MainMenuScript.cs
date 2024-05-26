f55124bb1126199912941cc13e0e31f3f64d8c6b
Grinik767 <grigorikiseleff@yandex.ru>
2024-05-26 23:39:46 +0500

    active buttons added

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
     private InputSystemUIInputModule _uiInput;
+    [SerializeField] private GameObject newGameButton;
     private void Start()
     {
         _uiInput = GetComponentInChildren<InputSystemUIInputModule>();
+        EventSystem.current.SetSelectedGameObject(newGameButton);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
